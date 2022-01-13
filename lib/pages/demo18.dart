21745c745c8dd92dcfdd51ad7879d6f2c3de8f94
chenjun <chenjun@orvibo.com>
2022-01-13 11:14:19 +0800

    *:带动画canvas

--- lib/pages/demo18.dart
+++ lib/pages/demo18.dart
@@ -2,37 +2,87 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 
-class Demo18 extends StatelessWidget {
+class Demo18 extends StatefulWidget {
   const Demo18({Key key}) : super(key: key);
+
+  @override
+  State<Demo18> createState() => _Demo18State();
+}
+
+class _Demo18State extends State<Demo18> with SingleTickerProviderStateMixin {
+  AnimationController spread;
+
+  @override
+  void initState() {
+    super.initState();
+    spread = AnimationController(vsync: this, duration: Duration(milliseconds: 2000))..repeat();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Demo18 page"),
-        ),
-        body: Column(
+      appBar: AppBar(
+        title: Text("Demo18 page"),
+      ),
+      body: SingleChildScrollView(
+        physics: new BouncingScrollPhysics(),
+        child: Column(
           children: [
-            Expanded(
-                child: Padding(
-                  padding: EdgeInsets.fromLTRB(0, 15, 0, 0),
-                  child: Text("Canvas基本绘制", style: TextStyle(fontSize: 18)),
-                ),
-                flex: 1),
-            Expanded(
-              child: Container(
-                width: double.infinity,
-                color: Colors.blueGrey,
-                child: CustomPaint(
-                  size: Size(double.infinity, 300),
-                  painter: MyCustomPainter(),
-                ),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 20),
+              child: Text("Canvas基本绘制", style: TextStyle(fontSize: 18)),
+            ),
+            Container(
+              width: double.infinity,
+              color: Colors.blueGrey,
+              child: CustomPaint(
+                size: Size(double.infinity, 300),
+                painter: MyCustomPainter(),
               ),
-              flex: 10,
-            )
+            ),
+            SizedBox(height: 40),
+            Container(
+              width: double.infinity,
+              color: Colors.blueGrey,
+              child: CustomPaint(
+                size: Size(double.infinity, 300),
+                painter: ShapePainter(spread: spread),
+              ),
+            ),
           ],
-        ));
+        ),
+      ),
+    );
   }
+
+  @override
+  void dispose() {
+    spread.dispose();
+    super.dispose();
+  }
+}
+
+class ShapePainter extends CustomPainter {
+  final Animation<double> spread;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final double smallRadius = size.width / 6;
+    final double spreadFactor = 2;
+    Paint paint = Paint()..color = Colors.green;
+    canvas.drawCircle(Offset(size.width / 2, size.height / 2), 10, paint);
+    if (spread.value != 0) {
+      paint.color = Colors.green.withOpacity(1 - spread.value);
+      canvas.drawCircle(Offset(size.width / 2, size.height / 2), spreadFactor * smallRadius * spread.value, paint);
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant ShapePainter oldDelegate) {
+    return oldDelegate.spread != spread;
+  }
+
+  ShapePainter({this.spread}) : super(repaint: spread);
 }
 
 class MyCustomPainter extends CustomPainter {
@@ -40,7 +90,6 @@ class MyCustomPainter extends CustomPainter {
   void paint(Canvas canvas, Size size) {
     double eWidth = size.width / 15;
     double eHeight = size.height / 15;
-    print("$eWidth,$eHeight");
     Paint paint = Paint()
       ..isAntiAlias = true
       ..style = PaintingStyle.fill
@@ -62,10 +111,18 @@ class MyCustomPainter extends CustomPainter {
     paint
       ..style = PaintingStyle.fill
       ..color = Colors.black;
-    canvas.drawCircle(Offset((size.width - eWidth) / 2, (size.height - eHeight) / 2), min(eWidth / 2, eHeight / 2) - 2, paint);
+    canvas.drawCircle(
+      Offset((size.width - eWidth) / 2, (size.height - eHeight) / 2),
+      min(eWidth / 2, eHeight / 2) - 2,
+      paint,
+    );
     // 画一个白子
     paint.color = Colors.white;
-    canvas.drawCircle(Offset(size.width / 2 + eWidth / 2, size.height / 2 - eHeight / 2), min(eWidth / 2, eHeight / 2) - 2, paint);
+    canvas.drawCircle(
+      Offset(size.width / 2 + eWidth / 2, size.height / 2 - eHeight / 2),
+      min(eWidth / 2, eHeight / 2) - 2,
+      paint,
+    );
   }
 
   // 在实际场景中正确利用此回调可以避免重绘开销，本示例我们简单的返回true
